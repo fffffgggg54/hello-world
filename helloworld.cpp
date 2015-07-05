20d03953c93207cf4d6a33701c3a8838cddf56f4
pi <pi@raspberrypi.local>
2015-07-05 20:57:02 +0000

    put main()'s content into a function

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,15 @@
 // Fredo's hello world
 #include  <stdio.h>
+int text()
+{
+  for(int l = 0;l < 100;l++){
+    printf("This line is: ");
+    printf("%3x or %3d ",l + 1, l + 1);
+    printf("hello world.\n");
+  }
+}
+
 int main()
 {
- for(int l = 0;l < 100;l++){
-  printf("This line is: ");
-  printf("%3x or %3d ",l + 1, l + 1);
-  printf("hello world.\n");
- }
+ text();
 }

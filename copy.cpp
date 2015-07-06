efdb3dd3188744341a813a2406a6a14f931bf54a
Fredo Guan <fredo.guan@hotmail.com>
2015-07-06 23:55:42 +0000

    add EOF

--- copy.cpp
+++ copy.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>  
 main()    
 {      
- int c;  
- while ((c = getchar()) != EOF)   {        putchar(c);    }
+ int c;
+ while (1)
+ {
+  int g = (c = getchar()) != EOF;
+  printf("g=%d\n", g);
+
+  if (g != 1) break;
+
+  putchar(c);
+ }
 } 

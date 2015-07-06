10ccabe00ff07da0b97db655041fae47dbd2a024
Fredo Guan <pi@raspberrypi.local>
2015-07-06 00:11:23 +0000

    added celcius conversion

--- fc-table.cpp
+++ fc-table.cpp
@@ -2,11 +2,13 @@
 int main()
 {
  float f, c;
- float l, u, s;
+ float l, u, s, uu, y;
  l = 0;
  u = 300;
+ y = 2;
  s = 20;
  f = l;
+ uu = 100;
  puts("conversion table");
 
  while (f <= u)
@@ -14,5 +16,13 @@ int main()
   c = 5 * (f-32) / 9;
   printf("%3.0f f %6.1f c\n", f, c);
   f = f + s;
+ }
+ puts("conversion table");
+ c = 0;
+ while(c <= uu)
+ {
+  f = c * 1.8 + 32;
+  printf("%3.0f c %6.1f f\n", c, f);
+  c = c + y;
  }
 }

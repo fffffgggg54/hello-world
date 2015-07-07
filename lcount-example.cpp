484a116f4ee1c43041324e4006dec1bdb6d9f7a3
Fredo Guan <fredo.guan@hotmail.com>
2015-07-07 19:48:55 +0000

    now does tab, enter, andspace

--- lcount-example.cpp
+++ lcount-example.cpp
@@ -5,7 +5,7 @@ main()
  int c, nl;  
  nl = 0;       
  while ((c = getchar()) != EOF)           
- if (c == '\n')               
+ if (c == '\n'||c == ' '||c == '\t')               
  ++nl;       
  printf("%d\n", nl);    
 } 

3942a715cfe1a49d5813f62513729a63d0d2d9cf
Fredo Guan <fredo.guan@hotmail.com>
2015-07-09 00:52:07 +0000

    prints each word

--- wcount.cpp
+++ wcount.cpp
@@ -4,19 +4,33 @@
    /* count lines, words, and characters in input */
 main()
 {
-    int c, nl, nw, nc, state;
+    int c, nl, nw, nc, state, cu, cua;
+    cu = cua = 0;
     state = OUT;
     nl = nw = nc = 0;
     while ((c = getchar()) != EOF) {
 	++nc;
 	if (c == '\n')
 	    ++nl;
-	if (c == ' ' || c == '\n' || c == '\t')
+	if (c == ' ' || c == '\n' || c == '\t') {
+
+            if(state == IN)
+            {
+             printf("\n");
+            }
 	    state = OUT;
+            
+            
+        } 
 	else if (state == OUT) {
 	    state = IN;
 	    ++nw;
+            
 	}
+	if(state == IN){
+	    printf("%c", c);
+        }
+
     }
     printf("%d lines %d words %d charactars \n", nl, nw, nc);
 }

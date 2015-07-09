b0ad9508ce95fd25f8f2ff116bdecbe08d2c57be
Fredo Guan <fredo.guan@hotmail.com>
2015-07-09 01:16:10 +0000

    removed excess stuff

--- wcount.cpp
+++ wcount.cpp
@@ -2,10 +2,9 @@
 #define IN   1			/* inside a word */
 #define OUT  0			/* outside a word */
    /* count lines, words, and characters in input */
-main()
+int main()
 {
-    int c, nl, nw, nc, state, cu, cua;
-    cu = cua = 0;
+    int c, nl, nw, nc, state;
     state = OUT;
     nl = nw = nc = 0;
     while ((c = getchar()) != EOF) {
@@ -13,22 +12,17 @@ main()
 	if (c == '\n')
 	    ++nl;
 	if (c == ' ' || c == '\n' || c == '\t') {
-
 	    if (state == IN) {
 		printf("\n");
 	    }
 	    state = OUT;
-
-
 	} else if (state == OUT) {
 	    state = IN;
 	    ++nw;
-
 	}
 	if (state == IN) {
 	    printf("%c", c);
 	}
-
     }
     printf("%d lines %d words %d charactars \n", nl, nw, nc);
 }

d76b7b4351085e0e797c479197c5a943b2babc68
Fredo Guan <fredo.guan@hotmail.com>
2015-07-10 23:36:49 +0000

    word lenth averager

---
+++ avrgwlenth.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define IN   1  /* inside a word */    
+#define OUT  0	/* outside a word */
+   /* count lines, words, and characters in input */ 
+void main ()
+{
+  int wordCount, charCount, state, keyInput, currLen;
+  state = OUT;
+  wordCount = charCount = currLen = 0;
+  while ((keyInput = getchar ()) != EOF)
+    {
+      ++charCount;
+      if (keyInput == ' ' || keyInput == '\n' || keyInput == '\t')
+      {
+	state = OUT;
+      }
+      else if (state == OUT)
+	{
+	  state = IN;
+          currLen = 0;
+	  ++wordCount;
+	}
+       if(state == IN)
+         {
+            currLen++;
+         }
+       
+    }
+  printf ("\n%d average\n", charCount / wordCount);
+}
+
+

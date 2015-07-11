84af177fe55ea628198c934e3992df8f197b91fc
Fredo Guan <fredo.guan@hotmail.com>
2015-07-11 00:21:23 +0000

    works as intended

--- avrgwlenth.c
+++ avrgwlenth.c
@@ -16,16 +16,22 @@ void main ()
       else if (state == OUT)
 	{
 	  state = IN;
-          charCount = charCount + (currLen + 1);
+          charCount = charCount + currLen;
           currLen = 0;
 	  ++wordCount;
+	  printf("[Debug] keyInput=0x%x, state transition from OUT to IN, wordCount=%d, charCount=%d, currLen=%d\n", keyInput, wordCount, charCount, currLen);		
 	}
        if(state == IN)
          {
             currLen++;
+	    printf("[Debug] keyInput=0x%x, state still IN or from OUT to IN, wordCount=%d, charCount=%d, currLen=%d\n", keyInput, wordCount, charCount, currLen);		
          }      
     }
-  
+
+  printf("[Debug] End of while. State is %s, wordCount=%d, charCount=%d, currLen=%d\n", state == IN ? "IN" : "OUT", wordCount, charCount, currLen);		
+
+  charCount = charCount + currLen;   
+  printf("[Debug] After final add. State is %s, wordCount=%d, charCount=%d, currLen=%d\n", state == IN ? "IN" : "OUT", wordCount, charCount, currLen);		
   printf ("\n%d average\n", charCount / wordCount);
 }
 
